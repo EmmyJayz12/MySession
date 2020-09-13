37385b0c28c64043901e645c7b26a9572b076ab1
Dominic <Dominic@DESKTOP-QKIJU43>
2020-09-13 22:07:06 +0100

    Initial Commit

---
+++ MySessionTest/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using MySessionTest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MySessionTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+           // Session["Username"] = "Sam";
+           // Session["UserId"] = 0123;
+
+            Session["user"] = new User() { Name = "Dominic", Email = "domebingonye@gmail.com", Password = "12345678" };
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            Session.Clear();
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
